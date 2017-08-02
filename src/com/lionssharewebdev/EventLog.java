248a051c1dcd017debe9ccf61231284d2eb977db
adrbin42 <adriennebing@me.com>
2017-08-02 06:01:07 -0400

    submit project,fixed errors

--- src/com/lionssharewebdev/EventLog.java
+++ src/com/lionssharewebdev/EventLog.java
@@ -6,28 +6,33 @@ import java.util.Scanner;
 public class EventLog {
     private List<Event> eventList;
 
-    public boolean addEvent(Event event){
-        try{
+    public boolean addEvent(Event event)throws IllegalArgumentException {
+
             System.out.print("Please enter the name of your event: ");
             Scanner scanner = new Scanner(System.in);
             String eventName = "";
             event.setName(scanner.next(eventName));
+            if(event.getName() == null ) {
+                throw new IllegalArgumentException("Please enter a name for your event!"); }
 
-            System.out.print("Please enter a description of what you will do at event: ");
+            System.out.print("Please enter one of the following event actions: Face2Face,PhoneCall,TextMessaging or Unknown");
             Scanner scanner2 = new Scanner(System.in);
             String eventAction = "";
             event.setAction(scanner.next(eventAction));
-
-            System.out.println("Information entered is as follows: " + eventName + " "+ eventAction);
+        if( event.getAction().equals("Face2Face") ||
+                event.getAction().equals("PhoneCall") ||
+                event.getAction().equals("TextMessaging") ||
+                event.getAction().equals("Unknown")
+                ) {
             eventList.add(event);
-            for (Event eventadded : eventList) {
-                System.out.println("Print event list "+ eventList);
-            }
-
-        }catch (IllegalArgumentException e){
-            System.out.print("Must add an event name and action!");
+            System.out.println("Information entered is as follows: " + event);
+            return true;
+        }else {
+            throw new IllegalArgumentException("Enter a proper event value: Face2Face,PhoneCall,TextMessaging or Unknown!");
         }
-        return true;
+
+
+
     }
 
     public int getNumEvents(){
